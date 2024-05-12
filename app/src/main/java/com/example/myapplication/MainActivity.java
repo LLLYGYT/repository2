dd8e7497b65c9a601cd53f5965f2f39849ee5e1c
xiaochuan <848047026@qq.com>
2024-05-12 20:43:21 +0800

    feat: 微调

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -12,22 +12,18 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
     ProgressBar progressBar;
     TextView textView;
-    int i=0;
-
-
-
+    int i = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        progressBar=findViewById(R.id.progressBar);//初始化
-        textView=findViewById(R.id.text);
-        final Timer timer =new Timer();//设置计时器，不懂可以去学一下线程
+        progressBar = findViewById(R.id.progressBar);//初始化
+        textView = findViewById(R.id.text);
+        final Timer timer = new Timer();//设置计时器，不懂可以去学一下线程
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
@@ -35,33 +31,22 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
 
-                            i++;
+                        i++;
 
-                            //i++;//每进行一次循环，i自增
-                            if(i==100){
-                                timer.cancel();//当i=100时，停止增加
+                        //i++;//每进行一次循环，i自增
+                        if (i == 100) {
+                            timer.cancel();//当i=100时，停止增加
 
-                            }else {
-                                progressBar.setProgress(i);//随i显示进度
-                                textView.setText("进度条值为："+i+"%");//显示当前进度
-                            }
-
-
-
-
-
-
-
-
-
-
-
+                        } else {
+                            progressBar.setProgress(i);//随i显示进度
+                            textView.setText("进度条值为：" + i + "%");//显示当前进度
+                        }
 
 
                     }
                 });
             }
-        },100,100);//每一秒循环一次
+        }, 100, 100);//每一秒循环一次
     }
 }
 
