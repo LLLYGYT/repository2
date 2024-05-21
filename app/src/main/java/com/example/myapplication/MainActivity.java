e6d15cdd22be2eb4803b7dc0fde1b9e19cbcfe62
ldh <1404552010@qq.com>
2024-05-21 23:08:16 +0800

    24-5-21-01

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,29 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
-
 public class MainActivity extends AppCompatActivity {
     int i = 0;
     int flag=0;
-
-
     Button button1;
 //    ProgressBar progressBar;
 //    TextView textView;
     ArrayList<String> shenglvehao=new ArrayList<String>();
-
-    String element=".";
-
-
-
+    String element1 =".";
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +27,62 @@ public class MainActivity extends AppCompatActivity {
         shenglvehao.add(".");
         shenglvehao.add("..");
         shenglvehao.add("...");
+
+        button1=findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //传参数给next页面
+                Intent intent1=new Intent(MainActivity.this,NextActivity.class);
+                intent1.putExtra("button-text",button1.getText().toString());
+                startActivityForResult(intent1,1000);
+                flag=1;
+            }
+        });
+        //变化的按钮文字
+        Timer timer = new Timer();//设置计时器，学一下线程
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {//将子线程传回到主线程更新到UI
+                    @Override
+                    public void run() {
+                        i++;//i++;//每进行一次循环，i自增
+                        if (i == 4) {
+                            i=0;
+                        } else {
+                            //progressBar.setProgress(i);//随i显示进度
+                            if(i==0){
+                                i=i+1;
+                            }
+                            if(flag==0)
+                            {
+                                element1 =shenglvehao.get(i-1);
+                                button1.setText("领域展开" + shenglvehao.get(i-1) );//显示当前进度
+                            }
+                        }
+                    }
+                });
+            }
+        }, 1000, 1000);//每一秒循环一次
+
+        //传参数给nextactivity
+//        Intent intent1=new Intent(this, NextActivity.class);
+//        intent1.putExtra("ELEMENT", element1);
+//        startActivity(intent1);
+
+
+        //按下按钮 页面的跳转
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, NextActivity.class);
+//                intent.putExtra("ELEMENT",button1.getText());
+//                startActivity(intent);
+//            }
+//        });
+//
+//
         //progressBar = findViewById(R.id.progressBar);//初始化
         /*textView = findViewById(R.id.text);*/
         /*  展示网页图片
@@ -71,81 +118,6 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         }, 100, 100);//每一秒循环一次*/
-        button1=findViewById(R.id.button1);
-
-
-
-
-
-        //按下按钮 页面的跳转
-//        button1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, NextActivity.class);
-//                intent.putExtra("ELEMENT",button1.getText());
-//                startActivity(intent);
-//            }
-//        });
-//
-//
-
-
-
-
-
-        //变化的按钮文字
-        Timer timer = new Timer();//设置计时器，学一下线程
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-
-                runOnUiThread(new Runnable() {//将子线程传回到主线程更新到UI
-                    @Override
-                    public void run() {
-                        i++;//i++;//每进行一次循环，i自增
-                        if (i == 4) {
-                            i=0;
-                        } else {
-                            //progressBar.setProgress(i);//随i显示进度
-                            if(i==0){
-                                i=i+1;
-                            }
-                            if(flag==0)
-                            {
-                                element=shenglvehao.get(i-1);
-                                button1.setText("领域展开" + shenglvehao.get(i-1) );//显示当前进度
-                            }
-
-                       }
-                    }
-                });
-            }
-        }, 1000, 1000);//每一秒循环一次
-
-
-
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,NextActivity.class);
-                intent.putExtra("button-text",button1.getText().toString());
-                startActivityForResult(intent,1000);
-                flag=1;
-            }
-        });
-
-
-        //传参数给nextactivity
-        Intent intent1=new Intent(this, NextActivity.class);
-        intent1.putExtra("ELEMENT",element);
-        startActivity(intent1);
-
-
-
-
-
-
     }
 }
 
--- app/src/main/java/com/example/myapplication/NextActivity.java
+++ app/src/main/java/com/example/myapplication/NextActivity.java
@@ -9,48 +9,31 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NextActivity extends AppCompatActivity {
-    int flag=0;
-
    @Override
     public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.next);
-
-
-
-
-       Button next_button_fanhui = findViewById(R.id.next_button_fanhui);
-
-       next_button_fanhui.setOnClickListener(new View.OnClickListener() {
-
+       Button button1 = findViewById(R.id.button2);
+       button1.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               Intent intent=new Intent(NextActivity.this,MainActivity.class);
-               intent.putExtra("name2",next_button_fanhui.getText().toString());
-               setResult(1001,intent);
+               Intent intent1=new Intent(NextActivity.this,MainActivity.class);
+               intent1.putExtra("name2",button1.getText().toString());
+               setResult(1001,intent1);
                finish();
-               flag=1;
-
-
-
            }
-
-
-
         });
-
-           Intent intent=getIntent();
-           Bundle bundle=getIntent().getExtras();
-           String textname=bundle.getString("button-text");
-           TextView textView=(TextView)findViewById(R.id.next_page);
-           String element=bundle.getString("button-text");
+           TextView textView=(TextView)findViewById(R.id.text5);
+           Intent intent2=new Intent();
+           Bundle bundle1=getIntent().getExtras();
+           String element=bundle1.getString("button-text");
            if(element!=null){
                textView.setText(element);
            }
            else{
                textView.setText(" ");
            }
-
        }
-
 }
+
+
